c4f45f54a09ff62657f49206598baa47ef84245b
deepak-maddhesiya <153506603+deepak-maddhesiya@users.noreply.github.com>
2023-12-11 16:16:48 +0530

    Add files via upload

---
+++ Radancy_assignment/Radancy_Code/Models/Account.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+namespace Radancy_Code.Models
+{
+    public class Account
+    {
+        /// <summary>
+        /// unique and application automatically create account number
+        /// </summary>
+        public int AccountNumber { get; set; }
+
+        /// <summary>
+        /// User first name
+        /// </summary>
+        public string? FirstName { get; set; }
+
+        /// <summary>
+        /// User last name
+        /// </summary>
+        public string? LastName { get; set; }
+        
+        /// <summary>
+        /// user account closing balance
+        /// </summary>
+        public decimal Balance { get; set; }
+
+    }
+}
