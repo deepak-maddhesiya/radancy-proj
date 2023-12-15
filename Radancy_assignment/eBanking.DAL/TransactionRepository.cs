b155c057d9d99a302e4f7c0f8dc59cc2a67bfabe
deepak-maddhesiya <153506603+deepak-maddhesiya@users.noreply.github.com>
2023-12-15 20:42:38 +0530

    Add files via upload

---
+++ Radancy_assignment/eBanking.DAL/TransactionRepository.cs
@@ -0,0 +1,41 @@
+﻿using eBanking.Domain.Abstractions;
+using eBanking.Domain;
+
+namespace eBanking.DAL
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        public Account DepositAmount(int accountNumber, decimal amount)
+        {
+            try
+            {
+                Account userAccount = In_MemoryData.accounts
+                    .FirstOrDefault(x => x.AccountNumber == accountNumber)!;
+
+                userAccount.Balance += amount;
+
+                return userAccount;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public Account WithdrawAmount(int accountNumber, decimal amount)
+        {
+            try
+            {
+                Account userAccount = In_MemoryData.accounts.FirstOrDefault(x => x.AccountNumber == accountNumber)!;
+
+                userAccount.Balance -= amount;
+
+                return userAccount;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
