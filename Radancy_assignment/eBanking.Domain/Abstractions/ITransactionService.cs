9c969e0be3424b4d29d3c0fd57eebebe13104b3a
deepak-maddhesiya <153506603+deepak-maddhesiya@users.noreply.github.com>
2023-12-19 14:45:14 +0530

    Delete Radancy_assignment directory

--- Radancy_assignment/eBanking.Domain/Abstractions/ITransactionService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace eBanking.Domain.Abstractions
-{
-    public interface ITransactionService
-    {
-        Account DepositAmount(int accountNumber, decimal amount);
-
-        Account WithdrawAmount(int accountNumber, decimal amount);
-    }
-}
